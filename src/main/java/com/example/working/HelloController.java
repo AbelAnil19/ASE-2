50b05d136c05f7e377064b87a03b874ae96d607d
workshop4u <workshop4u2026@gmail.com>
2026-09-14 15:34:13 +0530

    Add files via upload

---
+++ src/main/java/com/example/working/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.working;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String home() {
+        return """
+		<font color=red><h1>Welcome all
+		""";
+    }
+}
