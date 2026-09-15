4c227aff68553390d15e073f042e410904514cc4
workshop4u <workshop4u2026@gmail.com>
2026-09-15 20:16:26 +0530

    Update HelloController.java

--- src/main/java/com/example/working/HelloController.java
+++ src/main/java/com/example/working/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String home() {
         return """
 		<font color=red><h1>Welcome all
